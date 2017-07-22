0f5e52f7da9c68ba43f6d82293fc1cf2808dee95
Samarpan Chakraborty <samarpan93@gmail.com>
2017-07-22 12:45:19 +0530

    Initial Commit

---
+++ src/main/java/com/pandu/ResTservicesApplication.java
@@ -0,0 +1,12 @@
+package com.pandu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTservicesApplication.class, args);
+	}
+}
